ea5d5aa2d57e003656a3b16326e2827a82b346cb
chenrenbo <977872305@qq.com>
2019-08-27 18:04:46 +0800

    hyam 页面大纲

--- src/view/hardware/CancelStock.vue
+++ src/view/hardware/CancelStock.vue
@@ -29,32 +29,32 @@
             </el-form-item>
           </el-form>
         </el-card>
-        <el-col :span="24">
-          <el-button type="primary">主要按钮</el-button>
-          <el-button type="primary">主要按钮</el-button>
-          <el-button type="primary">主要按钮</el-button>
-          <el-button type="primary">主要按钮</el-button>
-          <el-button type="primary">主要按钮</el-button>
-          <el-button type="primary">主要按钮</el-button>
-        </el-col>
-        <el-col :span="24">
-          <el-card>
-            <el-table :data="tableData"
-                      style="width: 100%">
-              <el-table-column prop="date"
-                               label="日期"
-                               width="180">
-              </el-table-column>
-              <el-table-column prop="name"
-                               label="姓名"
-                               width="180">
-              </el-table-column>
-              <el-table-column prop="address"
-                               label="地址">
-              </el-table-column>
-            </el-table>
-          </el-card>
-        </el-col>
+      </el-col>
+      <el-col :span="24">
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+      </el-col>
+      <el-col :span="24">
+        <el-card>
+          <el-table :data="tableData"
+                    style="width: 100%">
+            <el-table-column prop="date"
+                             label="日期"
+                             width="180">
+            </el-table-column>
+            <el-table-column prop="name"
+                             label="姓名"
+                             width="180">
+            </el-table-column>
+            <el-table-column prop="address"
+                             label="地址">
+            </el-table-column>
+          </el-table>
+        </el-card>
       </el-col>
     </el-row>
   </div>
